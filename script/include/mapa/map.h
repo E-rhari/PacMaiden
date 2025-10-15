aa61b440a959bfb31b046a5f95ba0d034209e6fc
Joao-Miguel-S <jmsdealencar@gmail.com>
2025-10-15 15:57:25 -0300

    Update
    Finalizado a task de renderizar mapas

--- script/include/mapa/map.h
+++ script/include/mapa/map.h
@@ -4,60 +4,91 @@
 #include "raylib.h"
 
 
-typedef char Mapa[20][40];
-Mapa mapa;
+typedef char** Mapa;
 
-void renderizaMapa()
+void renderizaMapa(Mapa mapa)
 {
+    
     int cell = 40;
+
     for(int i = 0; i < 20; i++)
     {
         for(int j = 0; j < 40; j++)
         {
+
             switch(mapa[i][j])
             {
                 case '#':
-                    DrawRectangle(i * cell, j * cell, cell, cell, DARKBLUE);
+                    DrawRectangle(j * cell, i * cell, cell, cell, DARKBLUE);
                     break;
                 
                 case '.':
-                    DrawCircle(i * cell + cell/2, j * cell + cell/2, 1.5, WHITE);
+                    DrawRectangleLines(j * cell, i * cell, cell, cell, LIGHTGRAY);
+                    DrawCircle(j * cell + cell/2, i * cell + cell/2, 1.5, WHITE);
                     break;
                 
                 case 'o':
-                    DrawCircle(i * cell + cell/2, j * cell + cell/2, 3, GOLD);
+                    DrawCircle(j * cell + cell/2, i * cell + cell/2, 5, GOLD);
                     break;
                 
-                case 'T':
-                    DrawRectangle(i * cell, j * cell, cell, cell, PURPLE);
+                case 't':
+                    DrawRectangle(j * cell, i * cell, cell, cell, PURPLE);
                     break;
+                case 'P':
+                    DrawCircle(j * cell + cell/2, i * cell + cell/2, 10, GREEN);
+                    break;
+
             }
+            
         }
     }
+
 }
 
-void lerMapa (int nivel)
+void lerMapa (int nivel,Mapa mapa)
 {
 
-    char path[50]="../../../sprites/mapas/mapa";
-    char* nivelString;
+    char temp;
+
+    char path[50]="PacMaiden/sprites/mapas/mapa";
+    char nivelString[3];
 
     itoa(nivel,nivelString,10);
-    strcmp(path,nivelString);
-    strcmp(path,".txt");
+    strcat(path,nivelString);
+    strcat(path,".txt");
+
 
     FILE* arq = fopen(path, "r");
+
     if(arq == NULL)
     {
         printf("Erro de abertura de arquivo\n");
         return;
     }
-    for(int i = 0; i < 20; i++)
-        for(int j = 0; j < 40; j++)
-           mapa[i][j]= getc(arq);
 
+    
+    for(int i = 0; i < 20; i++){
+        for(int j = 0; j < 41; j++){
+            temp= getc(arq);
+            if(temp =='\n')
+                continue;
+            mapa[i][j]= temp;
+        }
+    }
     fclose(arq);
-
-    renderizaMapa();
 }
 
+Mapa setUpMapa(){
+    Mapa mapa=malloc(sizeof(char*)*20);
+
+    for(int i=0;i<20;i++)
+        *(mapa+i)=malloc(sizeof(char)*40);
+
+    return mapa;
+}
+
+void freeMapa(Mapa mapa){
+    for(int i=0; i<20;i++)
+        free(*(mapa+i));
+    free(mapa);
+}
