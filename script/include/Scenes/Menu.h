49e6bc2d12accd1b610c4c41ad736369094094d5
supermeganato <renatovm@dcc.ufrj.br>
2025-12-11 22:23:37 -0300

    REFACTOR: Menu de pause revigorado, animação de save
    Binoto fez tudo, eu só dei o merge
    
    Co-Authored-By: PedroBinoto <198611980+PedroBinoto@users.noreply.github.com>

--- script/include/Scenes/Menu.h
+++ script/include/Scenes/Menu.h
@@ -1,23 +1,22 @@
 #include "raylib.h"
 #include <stdbool.h>
 #include <time.h>
-#include "./TitleScreen.h"
 #include "../System/SaveSystem.h"
+#include "./TitleScreen.h"
 
 #pragma once
 
-#define BUTTONBASE (Color){10, 40, 80, 255}
-#define BUTTONHOVER (Color){20, 70, 140, 255}
-#define BUTTONBAR (Color){100, 180, 255, 255}
+#define BUTTONBASE BLACK
+#define BUTTONHOVER WHITE
+#define BUTTONBAR BLACK
 
-/** @brief representa a ação que um botão faz ao ser clicado*/
 enum optionPress{
     RESUME,
     SAVE,
     LOADSAVE,
     RETURN
 }; 
-/** @brief mapeamento do keycode das teclas, para, quando clicadas, desempenharam uma ação*/
+
 typedef enum KeyCode{
     S=83,
     C=67,
@@ -71,10 +70,6 @@ void gameStateExit(){
     gameState=EXIT;
 }
 
-/**
- * @return retorna se o mouse está sobre um botão
- */
-
 
 bool isOptionButtonHovered(OptionButton button, int buttonSelected) {
     if (isMouseMode()){
@@ -89,15 +84,63 @@ bool isSaveFileHovered(Rectangle save){
     return CheckCollisionPointRec(mousePos, save);
 }
 
-int isSaveFileClicked(Rectangle* save){
 
-  for(int i=0;i<3;i++)
-     if(isSaveFileHovered(save[i]) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        return i;
+
+int isSaveFileClicked(Rectangle* save)
+{
+    for (int i = 0; i < 3; i++)
+        if (isSaveFileHovered(save[i]) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            return i;
     return -1;
 }
 
-/** @brief inicializa os botões do menu */
+void isGameSaved(Rectangle* save){
+    static bool showSaved = false;
+    static float timer = 0.0f;
+
+    float dt = GetFrameTime();
+    const float duration = 3.0f;
+    const float fadeTime = 0.3f;
+
+    if (showSaved)
+    {
+        timer -= dt;
+        if (timer <= 0.0f)
+        {
+            timer = 0.0f;
+            showSaved = false;
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        if (isSaveFileHovered(save[i]) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            showSaved = true;
+            timer = duration;
+        }
+    }
+
+    if (showSaved)
+    {
+        float alpha = 1.0f;
+        if(timer > duration - fadeTime){
+            float t = (duration - timer) / fadeTime;
+            alpha = t;
+        }
+        else if(timer < fadeTime){
+            float t = timer / fadeTime;
+            alpha = t;
+        }
+        unsigned char finalAlpha = (unsigned char)(alpha * 255.0f);
+        Rectangle saved = {WIDTH/2 - 100, HEIGHT/2 - 50, 200, 100};
+        DrawRectangleRounded(saved, 0.3f, 10, (Color){0, 0, 0, finalAlpha});
+        DrawRectangleRoundedLines(saved, 0.3f, 10, RAYWHITE);
+        const char* savedText = "Jogo salvo";
+        Vector2 textSize = MeasureTextEx(GetFontDefault(), savedText, 18, 5);
+        DrawTextEx(GetFontDefault(), savedText, (Vector2){saved.x+(saved.width - textSize.x) / 2, saved.y+(saved.height - textSize.y)/2}, 18, 5, (Color){255, 255, 255, finalAlpha});
+    }
+}
+
 void initOptionButton(OptionButton *buttons){
     Vector4 optionMeasures = {700, 250, 200, 50};
     int paddingYButton=90;
@@ -107,13 +150,13 @@ void initOptionButton(OptionButton *buttons){
     buttons[2]=(OptionButton){(Rectangle){optionMeasures.x, optionMeasures.y+paddingYButton*2, optionMeasures.z, optionMeasures.w}, BUTTONBASE, BUTTONHOVER,2,gameStateLoading};
     buttons[3]=(OptionButton){(Rectangle){optionMeasures.x, optionMeasures.y+paddingYButton*3, optionMeasures.z, optionMeasures.w}, BUTTONBASE, BUTTONHOVER,3,gameStateExit};
 }
-/** @brief inicializa os botões do save */
+
 void initSaveButton(Rectangle *save){
-    Rectangle saveBox = {600, 175, 400, 450};
+    Rectangle saveBox = {650, 200, 300, 400};
     int saveRecx = 75;
     int saveRecy = 50;
     int savePicOffset = 50;
-    int padding = 75;
+    int padding = 50;
     
     for(int i=0; i<3;i++)
         save[i] = (Rectangle){saveBox.x + savePicOffset, saveBox.y + padding*(i+1) + saveRecy*i, saveRecx, saveRecy};
@@ -121,18 +164,26 @@ void initSaveButton(Rectangle *save){
 
 void drawOptionButtons(Rectangle menuBox, OptionButton *buttons, int buttonSelected){
     int  gapText=50, paddingYText=90;
+    Color textColor;
+    Color optionColor;
 
     const char *menuOptions[] = {"Retornar (TAB)", "Salvar (S)", "Carregar (C)", "Sair (Q)"};
 
     for(int i=0;i<4;i++){
         
-        Color optionColor = isOptionButtonHovered(buttons[i], buttonSelected) ? buttons[i].colorHover : buttons[i].colorBase;
+        if(isOptionButtonHovered(buttons[i], buttonSelected)){
+            optionColor = buttons[i].colorHover;
+            textColor = BUTTONBASE;
+        }
+        else{
+            optionColor = buttons[i].colorBase;
+            textColor = BUTTONHOVER;
+        }
 
         DrawRectangleRounded(buttons[i].optionBox, 0.2f, 10, optionColor);
-        DrawRectangleRoundedLinesEx(buttons[i].optionBox, 0.2f, 10, 3, BUTTONBAR);
 
         Vector2 optionSize = MeasureTextEx(GetFontDefault(), menuOptions[i], 18, 1);
-        DrawTextEx(GetFontDefault(), menuOptions[i], (Vector2){menuBox.x + (menuBox.width - optionSize.x) / 2, menuBox.y + gapText + (50 - optionSize.y) / 2}, 18, 1, BUTTONBAR);
+        DrawTextEx(GetFontDefault(), menuOptions[i], (Vector2){menuBox.x + (menuBox.width - optionSize.x) / 2, menuBox.y + gapText + (50 - optionSize.y) / 2}, 18, 1, textColor);
         gapText+=paddingYText;
     }
 
@@ -160,10 +211,10 @@ void drawOpenedMenu(OptionButton *buttons) {
     }
    
     Rectangle menuBox = {650, 200, 300, 400};
-    DrawRectangle(0, 0, WIDTH, HUDHEIGHT, (Color){0, 20, 60, 150});
-    DrawRectangleRounded(menuBox, 0.1f, 10, (Color){ 30, 80, 255, 255 });
+    DrawRectangleRounded(menuBox, 0.1f, 10, BLACK);
+    DrawRectangleRoundedLines(menuBox, 0.1f, 10, RAYWHITE);
     Vector2 textSize = MeasureTextEx(GetFontDefault(), "Menu", 18, 1);
-    DrawTextEx(GetFontDefault(), "Menu", (Vector2){menuBox.x + (menuBox.width - textSize.x) / 2, menuBox.y + (50 - textSize.y) / 2}, 18, 1, RAYWHITE);
+    DrawTextEx(GetFontDefault(), "Menu", (Vector2){menuBox.x + (menuBox.width - textSize.x) / 2, menuBox.y + (50 - textSize.y) / 2}, 20, 3, RAYWHITE);
     drawOptionButtons(menuBox, buttons, buttonSelected);
 
     if (isMouseMode()){
@@ -176,28 +227,31 @@ void drawOpenedMenu(OptionButton *buttons) {
             buttons[i].action();
 }
 
-void drawSaveStates(Rectangle* savePic){
-    Rectangle saveBox = {600, 175, 400, 450};
-    DrawRectangleRounded(saveBox, 0.1f, 10, (Color){ 30, 80, 255, 255 });
+void drawSaveStates(Rectangle*savePic){
+    Rectangle saveBox = {650, 200, 300, 400};
+    DrawRectangleRounded(saveBox, 0.1f, 10, BLACK);
+    DrawRectangleRoundedLines(saveBox, 0.1f, 10, RAYWHITE);
 
+    const char* backText = "Pressione TAB para voltar";
+    Vector2 textSize = MeasureTextEx(GetFontDefault(), backText, 18, 1);
+    DrawTextEx(GetFontDefault(), backText, (Vector2){saveBox.x + (saveBox.width - textSize.x) / 2, saveBox.y + saveBox.height - textSize.y - 20}, 18, 1, RAYWHITE);
     int textOffsetX = 95;
     int textOffsetY = 10;
 
-    for(int i = 0; i < 3; i++){
-        DrawRectangleRounded(savePic[i], 0.1f, 10, BLACK);
-        DrawRectangleRoundedLinesEx(savePic[i], 0.1f, 10, 3, BUTTONBAR);
+    Vector2 mousePos = GetMousePosition();
 
-        time_t now = time(NULL);
-        struct tm *infoTime = localtime(&now);
-        char saveMoment[32];
-        strftime(saveMoment, sizeof(saveMoment), "%d/%m/%Y %H:%M", infoTime);
+    for(int i = 0; i < 3; i++){
+        bool hovered = CheckCollisionPointRec(mousePos, savePic[i]);
+        Color saveColorHovered = hovered ? GRAY : BLACK;
+        DrawRectangleRounded(savePic[i], 0.1f, 10, saveColorHovered);
+        DrawRectangleRoundedLinesEx(savePic[i], 0.1f, 10, 3, BUTTONHOVER);
+
         char saveText[64];
-        snprintf(saveText, sizeof(saveText), "Save %d - %s", i + 1, saveMoment);
+        snprintf(saveText, sizeof(saveText), "Save %d", i + 1);
         DrawTextEx(GetFontDefault(), saveText,
                    (Vector2){savePic[i].x + textOffsetX, savePic[i].y + textOffsetY},
                    18, 1, RAYWHITE);
 
-        Vector2 mousePos = GetMousePosition();
+        isGameSaved(&savePic[i]);
     }
 }
-
